2dce5bc5aa1a1d4ca589c59ef76f1b9a6768769b
jsantore <jsantore@bridgew.edu>
2022-03-23 16:07:38 -0400

    checking for median income growth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import openpyxl
+import numbers
 
 
 def get_excel_sheet(filename):
@@ -12,9 +13,18 @@ def main():
     examine_data(income_data_sheet)
 
 def examine_data(income_sheet):
+    answer = int(input("What is the cutoff for income growth"))
+    print(f"States Whose Median Income Grew more than {answer} in 2015-2020")
     for row in income_sheet.rows:
         first_cell = row[0]
-        print(first_cell.value)
+        income_cell = row[1]
+        income_value = income_cell.value
+        if not isinstance(income_value, numbers.Number):
+            continue
+        income2015_cell = row[13]
+        income2015 = income2015_cell.value
+        if income_value-income2015 > answer:
+            print(f"{first_cell.value} : {income_value} old 2105 income {income2015}")
 
 
 main()
