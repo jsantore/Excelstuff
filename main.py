45b2449681a394338edafb454310d7b5c8aacf22
jsantore <jsantore@bridgew.edu>
2022-03-30 16:33:36 -0400

    end of Wednesday

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import openpyxl
 import numbers
 import openpyxl.utils
 import plotly.graph_objects
+from state_abbrev import us_state_to_abbrev
 
 
 def get_excel_sheet(filename):
@@ -17,24 +18,31 @@ def main():
 def examine_data(income_sheet):
     answer = int(input("What is the cutoff for income growth"))
     print(f"States Whose Median Income Grew more than {answer} in 2015-2020")
+    list_of_state_abrev = []
+    list_of_income_changes = []
     for row in income_sheet.rows:
         first_cell = row[0]
         income_cell = row[1]
         income_value = income_cell.value
         if not isinstance(income_value, numbers.Number):
             continue
-        #income2015_cell = row[13]
+        if first_cell.value not in us_state_to_abbrev:
+            continue
+        state_name = first_cell.value
+        state_abrev = us_state_to_abbrev[state_name]
+        list_of_state_abrev.append(state_abrev)
         income2015_cell_number = openpyxl.utils.cell.column_index_from_string('n')-1
         income2015_cell = row[income2015_cell_number]
         income2015 = income2015_cell.value
         income_change = income_value - income2015
-        income_chance_percent = income_change/income_value
-        income_chance_percent = income_chance_percent *100
-    
+        list_of_income_changes.append(income_change)
+        # income_chance_percent = income_change/income_value
+        # income_chance_percent = income_chance_percent *100
+
     map_to_show = plotly.graph_objects.Figure(
         data=plotly.graph_objects.Choropleth(
-            locations= " fill in here",
-            z= "fill in data here",
+            locations= list_of_state_abrev,
+            z= list_of_income_changes,
             locationmode="USA-states",
             colorscale='Jet',
             colorbar="fill in here soon"
